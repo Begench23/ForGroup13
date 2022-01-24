0961b38d4d6d130ab21f039769683c1446a1291b
misha <misha198620061@gmail.com>
2022-01-24 14:55:31 -0700

    Commit_3_StringReverse

--- src/misha/HelloWorld.java
+++ src/misha/HelloWorld.java
@@ -1,7 +1,7 @@
 package misha;
 
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String1[] args) {
         System.out.println("Hello");
         //1+2=3
         //23-1=23
